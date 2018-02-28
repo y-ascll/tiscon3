a5d2cd2964644bfe1067b0d3e0f2512b67ebfbb4
y-ascll <y.oaku490@gmail.com>
2018-02-28 09:56:02 +0900

    今朝分全部

--- src/main/java/jp/co/tis/tiscon3/form/CardOrderForm.java
+++ src/main/java/jp/co/tis/tiscon3/form/CardOrderForm.java
@@ -9,67 +9,67 @@ public class CardOrderForm extends FormBase {
 
     private static final long serialVersionUID = 1L;
 
-    @NotBlank
+    @NotBlank(message = "「漢字 姓」は必ず入力してください。")
     @Size(max = 60)
     private String kanjiName;
 
-    @NotBlank
+    @NotBlank(message = "「漢字 名」は必ず入力してください。")
     @Size(max = 60)
     private String kanjiName2;
 
-    @NotBlank
+    @NotBlank(message = "「カナ 姓」は必ず入力してください。")
     @Size(max = 90)
     @Pattern(regexp = "^[ァ-ヶー 　]*$")
     private String kanaName;
 
-    @NotBlank
+    @NotBlank(message = "「カナ 名」は必ず入力してください。")
     @Size(max = 90)
     @Pattern(regexp = "^[ァ-ヶー 　]*$")
     private String kanaName2;
 
-    @NotBlank
+    @NotBlank(message = "「ローマ字 姓」は必ず入力してください。")
     @Size(max = 120)
     @Pattern(regexp = "^[a-zA-Z 　]*$")
     private String alphabetName;
 
-    @NotBlank
+    @NotBlank(message = "「ローマ字 名」は必ず入力してください。")
     @Size(max = 120)
     @Pattern(regexp = "^[a-zA-Z 　]*$")
     private String alphabetName2;
 
 
-    @NotBlank
+    @NotBlank(message = "「生年月日」は必ず入力してください。")
     @Size(max = 10)
-    @Pattern(regexp = "\\d{4}/\\d{1,2}/\\d{1,2}$")
+    @Pattern(regexp = "\\d{4}/\\d{1,2}/\\d{1,2}$", message = "「生年月日」は例に従って正しく入力してください。")
     private String dateOfBirth;
 
-    @NotBlank
-    @Size(max = 3)
+    @NotBlank(message = "「年齢」は必ず入力してください。")
+    @Min(value = 18)
     private String age;
 
-    @NotBlank
+    @NotBlank(message = "「性別」は必ず選択してください。")
     @Size(max = 6)
     private String gender;
 
-    @NotBlank
+    @NotBlank(message = "「郵便番号」は必ず入力してください。")
     @Size(max = 8)
     @Pattern(regexp = "^[0-9]{3}-[0-9]{4}$")
     private String zipCode;
 
-    @NotBlank
+    @NotBlank(message = "「ご住所」は必ず入力してください。")
     @Size(max = 255)
     private String address;
 
-    @NotBlank
+    @NotBlank(message = "「電話番号１」は必ず入力してください。" )
     @Size(max = 13)
-    @Pattern(regexp = "^0[0-9]{1,3}-[0-9]{2,4}-[0-9]{4}$")
+    @Pattern(regexp = "^0[0-9]{1,3}-[0-9]{2,4}-[0-9]{4}$|^((070|080|090)-[0-9]{4}-[0-9]{4})?$")
     private String homePhoneNumber;
 
     @Size(max = 13)
     @Pattern(regexp = "^((070|080|090)-[0-9]{4}-[0-9]{4})?$")
     private String mobilePhoneNumber;
 
-    @NotBlank
+    @NotBlank(message = "「メールアドレス」は必ず入力してください。" )
     @Size(max = 255)
     @Email
     private String emailAddress;
